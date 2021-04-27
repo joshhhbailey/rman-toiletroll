d857077a0667edf2eb4cc1e8eec889a221e8d531
Josh Bailey <65401458+JoshhhBailey@users.noreply.github.com>
2021-04-27 13:56:11 +0100

    Lighting and circles pattern adjustments. Display setup for denoising

--- toiletroll.py
+++ toiletroll.py
@@ -3,49 +3,45 @@ import sys
 import sys, os.path, subprocess
 import random
 
+# The following function is adapted from:
+# Macey, J,. 2020. Introduction to Renderman and Python. [online]
+# Available from: https://nccastaff.bournemouth.ac.uk/jmacey/msc/renderman/lectures/Lecture1/
+# Accessed [23 March 2021]
 def CreateCube(_width = 1.0, _height = 1.0, _depth = 1.0):   
-    # The following function is adapted from:
-    # Macey, J,. 2020. Introduction to Renderman and Python. [online]
-    # Available from: https://nccastaff.bournemouth.ac.uk/jmacey/msc/renderman/lectures/Lecture1/
-    # Accessed [23 March 2021]
     w = _width / 2.0
     h = _height / 2.0
     d = _depth / 2.0
 
     # Apply texture
-    ri.Pattern('table','table', 
+    ri.Pattern("table","table",                                     # Texture acquired from:      
+    {                                                               # Texture Haven,. 2021. plywood [online]
+    	"string path" : ["images/textures/plywood_diff_1k.tx"]      # Available from: https://texturehaven.com/tex/?c=wood&t=plywood
+    })                                                              # Accessed [15 April 2021]
+    ri.Bxdf("PxrSurface", "wood",
     {
-        # Texture acquired from:
-        # Texture Haven,. 2021. plywood [online]
-        # Available from: https://texturehaven.com/tex/?c=wood&t=plywood
-        # Accessed [15 April 2021]
-    	'string path' : ["images/textures/woodtest.tx"]
-    })
-    ri.Bxdf('PxrSurface', 'wood',
-    {
-    	'reference color diffuseColor' : ['table:resultRGB'],
-    	'int diffuseDoubleSided' : [1],
-    	'float reflectionGain' : [0.2]
+    	"reference color diffuseColor" : ["table:resultRGB"],
+    	"int diffuseDoubleSided" : [1],
+    	"float reflectionGain" : [0.2]
 	})
 
     # Rear
     face = [-w, -h, d, -w, h, d, w, -h, d, w, h, d]                                
-    ri.Patch("bilinear", {'P':face})
+    ri.Patch("bilinear", {"P":face})
     # Front
     face = [-w, -h, -d, -w, h, -d, w, -h, -d, w, h, -d]                                
-    ri.Patch("bilinear", {'P':face})
+    ri.Patch("bilinear", {"P":face})
     # Left
     face = [-w, -h, -d, -w, h, -d, -w, -h, d, -w, h, d]                                    
-    ri.Patch("bilinear", {'P':face})
+    ri.Patch("bilinear", {"P":face})
     # Right
     face = [w, -h, -d, w, h, -d, w, -h, d, w, h, d]                                
-    ri.Patch("bilinear", {'P':face})
+    ri.Patch("bilinear", {"P":face})
     # Bottom
     face = [w, -h, d, w, -h, -d, -w, -h, d, -w, -h, -d]                                
-    ri.Patch("bilinear", {'P':face})
+    ri.Patch("bilinear", {"P":face})
     # Top
     face = [w, h, d, w, h, -d, -w, h, d, -w, h, -d]                                
-    ri.Patch("bilinear", {'P':face})
+    ri.Patch("bilinear", {"P":face})
 
 def CreateRoll(_height = 1.0, _outerRadius = 1.0, _innerRadius = 0.5, _pattern = "tissuePatternWave"):
     # Inner tube
@@ -112,25 +108,25 @@ def CreateRoll(_height = 1.0, _outerRadius = 1.0, _innerRadius = 0.5, _pattern =
 
     # Top / Bottom
     ri.AttributeBegin()
-    ri.Attribute('displacementbound', 
+    ri.Attribute("displacementbound", 
     {
-        'sphere' : [1],
-        'coordinatesystem' : ['shader']
+        "sphere" : [1],
+        "coordinatesystem" : ["shader"]
     })
     noiseHeight = random.uniform(0.003, 0.005)
     noiseFreq = random.randint(195, 205)
-    ri.Pattern('tissueNoise', 'tissueNoise',
+    ri.Pattern("tissueNoise", "tissueNoise",
     {
         "float height" : [noiseHeight],
         "int frequency" : [noiseFreq]
     })
-    ri.Displace('PxrDisplace', 'myDisp',
+    ri.Displace("PxrDisplace", "myDisp",
     {
-        'reference float dispScalar' : ['tissueNoise:dispOut']
+        "reference float dispScalar" : ["tissueNoise:dispOut"]
     })
-    ri.Bxdf('PxrSurface', 'top',
+    ri.Bxdf("PxrSurface", "top",
     {
-        'reference color diffuseColor' : ['tissueNoise:resultRGB'],
+        "reference color diffuseColor" : ["tissueNoise:resultRGB"],
         "int diffuseDoubleSided" : [1],
         "float diffuseRoughness" : [1.0],
         "float fuzzConeAngle" : [8.0],
@@ -169,17 +165,55 @@ def CreateRollPyramid(_layers, _height = 1.0, _outerRadius = 1.0, _innerRadius =
         # Move to centre of next layer
         ri.Translate(-(offset * _outerRadius * 2) + _outerRadius, 0, -_height * 2)
 
+# The following function is from:
+# Macey, J., 2018. Lecture4Shaders. [online]
+# Available from: https://github.com/NCCA/Renderman/blob/master/Lecture4Shaders/Bands/bands.py
+# Accessed [25 March 2021]
 def CompileShader(_shader):
-    # The following function is from:
-    # Macey, J., 2018. Lecture4Shaders. [online]
-    # Available from: https://github.com/NCCA/Renderman/blob/master/Lecture4Shaders/Bands/bands.py
-    # Accessed [25 March 2021]
   	if os.path.isfile(_shader + ".oso") != True or os.stat(_shader + ".osl").st_mtime - os.stat(_shader + ".oso").st_mtime > 0:
 	    print("compiling shader %s" %(_shader))
 	    try:
 		    subprocess.check_call(["oslc", _shader + ".osl"])
 	    except subprocess.CalledProcessError:
-		    sys.exit('shader compilation failed')
+		    sys.exit("shader compilation failed")
+
+def SetupDisplay():
+    # Beauty...
+    ri.DisplayChannel("color Ci")
+    ri.DisplayChannel("float a")
+    ri.DisplayChannel("color mse",
+        {"string source" : "color Ci", "string statistics" : "mse"})
+
+    # Shading...
+    ri.DisplayChannel("color albedo",
+        {"string source" : "color lpe:nothruput;noinfinitecheck;noclamp;unoccluded;overwrite;C<.S'passthru'>*((U2L)|O)"})
+    ri.DisplayChannel("color albedo_var",
+        {"string source" : "color lpe:nothruput;noinfinitecheck;noclamp;unoccluded;overwrite;C<.S'passthru'>*((U2L)|O)", "string statistics" : "variance" } )
+    ri.DisplayChannel("color diffuse",
+        {"string source" : "color lpe:C(D[DS]*[LO])|O" })
+    ri.DisplayChannel("color diffuse_mse",
+        {"string source" : "color lpe:C(D[DS]*[LO])|O", "string statistics" : "mse"})
+    ri.DisplayChannel("color specular",
+        {"string source" : "color lpe:CS[DS]*[LO]"})
+    ri.DisplayChannel("color specular_mse",
+        {"string source" : "color lpe:CS[DS]*[LO]", "string statistics" : "mse"})
+
+    # Geometry...
+    ri.DisplayChannel("float z",
+        {"string source" : "float z" ,"string filter": "gaussian"})
+    ri.DisplayChannel("float z_var",
+        {"string source" : "float z" ,"string filter" : "gaussian" ,"string statistics": "variance"})
+    ri.DisplayChannel("normal normal",
+        {"string source" : "normal Nn"})
+    ri.DisplayChannel("normal normal_var",
+        {"string source" : "normal Nn", "string statistics" : "variance"})
+    ri.DisplayChannel("vector forward",
+        {"string source"  : "vector motionFore"})
+    ri.DisplayChannel("vector backward",
+        {"string source" : "vector motionBack"})
+
+    # framebuffer or openexr
+    ri.Display("toiletroll.exr", "framebuffer", "Ci,a,mse,albedo,albedo_var,diffuse,diffuse_mse,specular,specular_mse,z,z_var,normal,normal_var,forward,backward", { "int asrgba" : [1]})
 
 if __name__ == "__main__":
     CompileShader("shaders/tissuePatternCircles")
@@ -191,17 +225,18 @@ if __name__ == "__main__":
     ri = prman.Ri()     # Create RenderMan interface instance
 
     ri.Begin("__render")    # Begin .rib and pass to renderer
-    ri.Display("toiletroll.exr", "framebuffer", "rgba")       # File, Buffer, Colour Channels
-    ri.Format(1024, 1024, 1)                              # Width, Height, Aspect Ratio
+    SetupDisplay()  # Denoising
+    ri.Format(1920, 1080, 1)                              # Width, Height, Aspect Ratio
 
-    ri.Hider("raytrace", {"int incremental" :[1]})
+    ri.Hider("raytrace", {"int incremental" :[1], "string pixelfiltermode" : "importance"})
     ri.Integrator("PxrPathTracer", "integrator")
 
     # Camera coordinate system
     ri.Projection(ri.PERSPECTIVE)
-    ri.Translate(0, -2, 5)
-    ri.Rotate(70, 1, 0, 0)
-    #ri.Rotate(-10, 0, 0, 1)
+    ri.DepthOfField(2.0, 0.15, 2.5)
+
+    ri.Translate(0, -1, 3.75)
+    ri.Rotate(75, 1, 0, 0)
 
     # World coordinate system
     ri.WorldBegin()
@@ -214,7 +249,7 @@ if __name__ == "__main__":
     roll_outerRadius = 1.04
     roll_innerRadius = 0.49
     #CreateRoll(roll_height, roll_outerRadius, roll_innerRadius, "tissuePatternCircles")
-    CreateRollPyramid(2, 1.06, 1.04, 0.49, "random")
+    CreateRollPyramid(2, 1.06, 1.04, 0.49, "tissuePatternCircles")
     ri.AttributeEnd()
     ri.TransformEnd()
 
@@ -236,14 +271,10 @@ if __name__ == "__main__":
     ri.Attribute ("identifier", {"name": "Environment Map"})
     ri.Rotate(180, 1, 0, 0)
     ri.Rotate(-66, 0, 0, 1)
-    ri.Light("PxrDomeLight", "EnvMapLight",
-    {
-        "float exposure" : [-0.5],
-        # HDRI acquired from:
-        # HDRI Haven,. 2021. Lebombo [online]
-        # Available from: https://hdrihaven.com/hdri/?c=indoor&h=lebombo
-        # Accessed [21 April 2021]
-        "string lightColorMap" : ["images/env-map/lebombo_4k.tx"]
+    ri.Light("PxrDomeLight", "EnvMapLight",                         # HDRI acquired from:
+    {                                                               # HDRI Haven,. 2021. Lebombo [online]
+        "float exposure" : [-0.25],                                 # Available from: https://hdrihaven.com/hdri/?c=indoor&h=lebombo
+        "string lightColorMap" : ["images/env-map/lebombo_4k.tx"]   # Accessed [21 April 2021]
     })
     ri.AttributeEnd()
     ri.TransformEnd()
